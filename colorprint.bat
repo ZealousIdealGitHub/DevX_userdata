6af9ccaa1fe8351bba62c7e0c82db0c9e22ffa9c
ZealousIdealGitHub <134780581+ZealousIdealGitHub@users.noreply.github.com>
2025-09-06 19:24:33 +0530

    Upload colorprint.bat

---
+++ colorprint.bat
@@ -0,0 +1,4 @@
+echo <font color="green">Green Text</font> & echo <font color="blue">Blue Text</font> # using batch command to print colored text in Windows 
+# Note: This might not work in all Windows terminals, color support varies 
+# For a more reliable solution, consider using PowerShell: 
+# Write-Host "Green Text" -ForegroundColor Green; Write-Host "Blue Text" -ForegroundColor Blue # using PowerShell to print colored text
